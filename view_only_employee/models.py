3833ff46fd4ee9a80ac8371e33400a305c09115a
Tlaloc-Es <Tlaloc-Es@users.noreply.github.com>
2017-11-28 16:28:25 +0100

    Update models.py
    Corregidos fallos

--- view_only_employee/models.py
+++ view_only_employee/models.py
@@ -3,5 +3,4 @@ from openerp import models, fields, api
 
 class view_only_employee(models.Model):
 	_inherit = 'res.partner'
-	employeeID = fields.Many2one('hr.employee', ondelete='set null', string="Empleado", help='Indica a que empleado pertenece este contacto y lo oculta para todos los empleados.
-		', index=True)
+	employeeID = fields.Many2one('hr.employee', ondelete='set null', string="Empleado", help='Indica a que empleado pertenece este contacto y lo oculta para todos los empleados.', index=True)
